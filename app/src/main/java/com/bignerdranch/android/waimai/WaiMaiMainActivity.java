68beafbbf2145a24be910356f99dd950d25088b1
Warren Zhang <warren@cincoai.com>
2017-08-28 00:30:47 -0700

    first checkin skeleton code

---
+++ app/src/main/java/com/bignerdranch/android/waimai/WaiMaiMainActivity.java
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.waimai;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WaiMaiMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wai_mai_main);
+    }
+}
